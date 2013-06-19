1fc933fd1d2edb0378587f12567aa62c960df0d9
Robert Maes <remilmaes@gmail.com>
2013-06-19 14:45:44 -0500

    Add commands and their functions

--- SimpleEconomy/src/io/github/kinglime/simpleeconomy/SimpleEconomy.java
+++ SimpleEconomy/src/io/github/kinglime/simpleeconomy/SimpleEconomy.java
@@ -5,9 +5,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,7 +21,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class SimpleEconomy extends JavaPlugin implements Listener {
 	
 	public HashMap <String, Double> accounts;
-	
+	public char currencyType;
 	
 	public HashMap<String, Double> readMap() throws IOException {
 		HashMap <String, Double> readAccounts = new HashMap <String, Double>();
@@ -64,6 +68,7 @@ public class SimpleEconomy extends JavaPlugin implements Listener {
 				accounts.put(player.getName(), getConfig().getDouble("SimpleEconomy.StartAmount"));
 			}
 		}
+		currencyType = getConfig().getString("SimpleEconomy.CurrencyChar").charAt(0);
 	}
 	
 	public void onDisable() {
@@ -77,14 +82,104 @@ public class SimpleEconomy extends JavaPlugin implements Listener {
 	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
-		getLogger().info("Player joined the game");
 		Player player = event.getPlayer();
-		getLogger().info(player.getName());
 		if (!accounts.containsKey(player.getName())) {
-			getLogger().info("adding name to list");
 			accounts.put(player.getName(), getConfig().getDouble("SimpleEconomy.StartAmount"));
+		}
+	}
+	
+	public Player guessPlayer(String name) {
+		List<Player> player = Bukkit.getServer().matchPlayer(name);
+		int size = player.size();
+		if (size == 1) {
+			return player.get(0);
 		} else {
-			getLogger().info("not adding name to list");
+			return null;
+		}
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("money")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("This command can only be run by a player.");
+				return false;
+			} else {
+				Player player = (Player) sender;
+				player.sendMessage("Balance: " + currencyType + getBalance(player));
+				return true;
+			}
+		} else if (cmd.getName().equalsIgnoreCase("pay")) {
+			if (!(sender instanceof Player)) {
+				sender.sendMessage("This command can only be run by a player.");
+				return false;
+			} else {
+				Player player = (Player) sender;
+				if (args.length == 2) {
+					String payTo = args[0];
+					double amount;
+					try {
+						amount = Double.parseDouble(args[1]);
+					}
+					catch (NumberFormatException e) {
+						player.sendMessage("The amount must be a valid number");
+						return false;
+					}
+					if (amount > 0 && amount <= getBalance(player)) {
+						Player playerToSendTo = guessPlayer(payTo);
+						if (playerToSendTo != null) {
+							if (playerToSendTo.getName().equals(player.getName())) {
+								player.sendMessage("You cannot send money to your self.");
+								return false;
+							} else {
+								if ((changeBalance(true, amount, playerToSendTo) == true) && (changeBalance(false, amount, player))) {
+									player.sendMessage("Sent " + currencyType + amount + " to " + playerToSendTo.getName() + ".");
+									playerToSendTo.sendMessage(player.getName() + " sent you " + currencyType + amount + ".");
+									return true;
+								} else {
+									player.sendMessage("Error sending money.");
+									return false;
+								}
+								}
+						} else {
+							player.sendMessage("Can't find player " + payTo + ".");
+							return false;
+						}
+					} else {
+						player.sendMessage("Insufficient funds.");
+						return false;
+					}
+				} else {
+					sender.sendMessage("Not enough arguments");
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+	
+	public double getBalance(Player player) {
+		double balance = accounts.get(player.getName());
+		return balance;
+	}
+	
+	public boolean changeBalance(boolean positive, double amount, Player player) {
+		if (accounts.get(player.getName()) != null) {
+			double balance = accounts.get(player.getName());
+				double newBalance = 0;
+				if (positive == true) {
+					newBalance = balance + amount;
+				} else if (positive == false) {
+					newBalance = balance - amount;
+				}
+				if (newBalance >= 0) {
+					accounts.put(player.getName(), newBalance);
+					return true;
+				} else {
+					return false;
+				}
+		} else {
+			return false;
 		}
 	}
 }
